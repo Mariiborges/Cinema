61d3815b9ca06b38be857ac4306953ea8c792659
Mariiborges <maryclara.ssantos@gmail.com>
2025-05-01 20:28:30 -0300

    Adicionando estrutura inicial,layout, menu lateral e rotas

---
+++ Cinema/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import AppLayout from './components/Layout';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout></AppLayout>} />
+
+    </Routes>
+    
+  );
+}
+
+export default App;
